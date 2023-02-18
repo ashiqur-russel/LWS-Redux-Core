fc73720ba8ecbe5d67779550e726625ef8726d49
ashiqurras <ashiqurrussel2@gmail.com>
2023-02-18 13:39:06 +0100

    counter app one way data flow view->action->state->view->actiom-state->

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -1,14 +1,31 @@
-import React from "react";
+import React, { useState } from "react";
 
 const Counter = () => {
+  const [count, setCount] = useState(0);
+
+  const increment = () => {
+    setCount((prevCount) => prevCount + 1);
+  };
+  const decrement = () => {
+    if (count === 0) {
+      return;
+    }
+    setCount((prevCount) => prevCount - 1);
+  };
   return (
     <div class="p-4 h-auto flex flex-col items-center justify-center space-y-5 bg-white rounded shadow">
-      <div class="text-2xl font-semibold">0</div>
+      <div class="text-2xl font-semibold">{count}</div>
       <div class="flex space-x-3">
-        <button class="bg-indigo-400 text-white px-3 py-2 rounded shadow">
+        <button
+          onClick={increment}
+          class="bg-indigo-400 text-white px-3 py-2 rounded shadow"
+        >
           Increment
         </button>
-        <button class="bg-red-400 text-white px-3 py-2 rounded shadow">
+        <button
+          onClick={decrement}
+          class="bg-red-400 text-white px-3 py-2 rounded shadow"
+        >
           Decrement
         </button>
       </div>
