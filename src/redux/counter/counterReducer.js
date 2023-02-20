305cc2e6e91de7594987061afe9e2421ccbe5853
ashiqurras <ashiqurrussel2@gmail.com>
2023-02-20 22:09:35 +0100

    reducer function created

---
+++ src/redux/counter/counterReducer.js
@@ -0,0 +1,26 @@
+import { DECREMNET, INCREMENT } from "./actionTypes";
+
+const initialState = {
+  value: 0,
+};
+const counterReducer = (state = initialState, action) => {
+  switch (action.type) {
+    case INCREMENT: {
+      return {
+        ...state,
+        value: state.value + 1,
+      };
+    }
+    case DECREMNET: {
+      return {
+        ...state,
+        value: state.value - 1,
+      };
+    }
+
+    default:
+      return state;
+  }
+};
+
+export default counterReducer;
